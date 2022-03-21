2f2fe4c1c2d7cd135d6b1d30fc5eb81b14683524
BavKudhail <93915846+BavKudhail@users.noreply.github.com>
2022-03-21 13:03:47 +0000

    built get and delete routes for product-routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -25,11 +25,18 @@ router.get("/:id", async (req, res) => {
     const productData = await Product.findByPk(req.params.id, {
       include: [{ model: Category }, { model: Tag }],
     });
-  } catch (error) {}
+    if (!productData) {
+      res.status(404).json({ message: "No product data found with that id!" });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 // create new product
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -102,8 +109,22 @@ router.put("/:id", (req, res) => {
     });
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+    if (!productData) {
+      res.status(404).json({ message: "No product found with that id!" });
+      return;
+    }
+    res.status(200).json(productData);
+  } catch (error) {
+    res.status(500).json(error);
+  }
 });
 
 module.exports = router;
